aec404a11d287e5a010a084321de9ddd3ab3ad07
jmzcayetano <jmzcayetano@gmail.com>
2024-10-08 12:45:58 -0500

    Se agregan variables a Casa

--- src/main/java/com/example/restinventario/entity/Casa.java
+++ src/main/java/com/example/restinventario/entity/Casa.java
@@ -2,4 +2,33 @@ package com.example.restinventario.entity;
 
 public class Casa {
 
+	private Long id;
+	private String description;
+
+	public Casa() {
+		super();
+	}
+
+	public Casa(Long id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
